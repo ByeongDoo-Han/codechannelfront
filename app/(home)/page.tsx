12bc4558b2bd3abe25574dede265dcb48d6c1247
byeongdoohan <quden04@gmail.com>
2025-07-27 01:20:51 +0900

    서버 컴포넌트 변경

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Content from "./content";
 import ReactQueryProvider from "../provider/ReactQueryProvider";
 
-export default async function Home() {
-
-    async function fetchStudies() {
-        const response = await fetch("http://54.180.132.97:8080/api/v1/studies");
-        const data = await response.json();
-        return data;
-    }
-    const studies = await fetchStudies();
+export default function Home() {
+    const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
+    const [studies, setStudies] = useState([]);
     
-
+    useEffect(() => {
+        const fetchStudies = async () => {
+            const response = await fetch(`${API_HOST}/api/v1/studies`);
+            const data = await response.json();
+            setStudies(data);
+        }
+        fetchStudies();
+    }, []);
     return (
         <ReactQueryProvider>
             <Content studies={studies}/>
